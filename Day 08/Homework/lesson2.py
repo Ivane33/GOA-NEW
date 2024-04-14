034bf4784bbb72b994c96f2cc3a3a3a9c61fc973
Ivane33 <ivaneina123@gmail.com>
2024-04-14 13:33:29 +0400

    commit

---
+++ Day 08/Homework/lesson2.py
@@ -0,0 +1,14 @@
+i = 0
+while i < 101:
+  print(i)
+  i += 1
+
+number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+for y in number:
+  print(y)
+
+
+
+for i in range(2, 22, 2): 
+ print(i)
+
---
+++ Day 10/Homework/lesson.py
@@ -0,0 +1,16 @@
+for i in range(0, 7):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
